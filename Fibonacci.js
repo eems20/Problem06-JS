b6d7b9e66578f5fbcb93f1a02c2b509a46294e72
erving <marureso@unlv.nevada.edu>
2020-10-02 15:33:12 -0700

    added fibonnaci method

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,8 +1,16 @@
 class Fibonacci {
-    getFibonacciValue(a, b){
-        let divisor = -1;
-
-        return divisor;
+    getFibonacciValue(a){
+       // let divisor = -1;
+       // testing initial conditinos of a is 0 or 1, return that
+        if (a == 0 ){
+            return 0;
+        }
+        if (a == 1 ){
+            return 1;
+        }
+        // else return the fibonacci calculation 
+        return this.getFibonacciValue(a-1) + this.getFibonacciValue(a-2);
+       // return divisor;
     }
 }
 
